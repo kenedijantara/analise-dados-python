476bb51c34f5bbd876052f5225076184d933afdf
kenedijantara <kenedijantara@gmail.com>
2026-07-17 22:54:00 -0300

    feat: implementa pipeline de extração de dados em lotes para a camada raw

---
+++ sql/0_criar_banco.sql
@@ -0,0 +1,9 @@
+-- Projeto: Análise de Dados com Python e MySQL
+-- Autor: Kenedi Jantara
+-- Descrição: Criação do banco de dados do projeto.
+
+DROP DATABASE IF EXISTS analise_dados;
+
+CREATE DATABASE analise_dados;
+
+USE analise_dados;
